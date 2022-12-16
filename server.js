adbe7e69dfac35ca9e3dc26502bc40efb7f2ce08
Luis Bustos <luisbustos500@gmail.com>
2022-12-16 12:27:45 -0700

    added create route

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ const logs = require('./models/logs.js');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
+app.use(express.urlencoded({extended:false}));
+
 
 //New Route
 app.get('/logs/new', (req, res) => {
     res.render('New');
 });
 
+//Create Route
+app.post('/logs', (req, res) => {
+    req.body.shipIsBroken = req.body.shipIsBroken === 'on' ? true : false;
+    res.send(req.body);
+})
 
 
 
